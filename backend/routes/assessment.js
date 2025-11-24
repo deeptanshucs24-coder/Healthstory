2ae57ebb16a1b705254cbac0c106d17bcf6be3fc
deeptanshucs24-coder <deeptanshu.cs24@bmsce.ac.in>
2025-11-24 12:01:01 +0530

    Delete backend directory

--- backend/routes/assessment.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { analyzeAssessment, generateReport } = require('../controllers/assessmentController');
-
-router.post('/analyze', analyzeAssessment);
-router.post('/download', generateReport);
-
-module.exports = router;
